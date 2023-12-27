5d0a3d918e02ce68aaf9ffbae86ec2b71adf8fab
Soham <63231620+sohamk3901@users.noreply.github.com>
2023-12-27 12:56:16 +0530

    Featured Post bug fix

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -12,7 +12,7 @@ const roboto = Roboto({
 const FeaturedPostCard = ({ post }) => (
   <div className={`${roboto.className} relative h-80`}>
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-full" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
-    <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-neutral-800 to-black w-full h-72" />
+    <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-black to-neutral-800 w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
       <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('DD MMM, YYYY')}</p>
       <p className="text-white mb-4 text-shadow font-semibold text-xl text-center">{post.title}</p>
