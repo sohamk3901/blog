733b7cf91c06313fe987150e8d8fc014f17e8202
Soham <63231620+sohamk3901@users.noreply.github.com>
2024-01-14 00:20:20 +0530

    List changes

--- components/LaunchWidget.jsx
+++ components/LaunchWidget.jsx
@@ -20,7 +20,7 @@ const LaunchWidget = () => {
   return (
     <div className="shadow-lg rounded-lg p-8 pb-12 mb-8 bg-slate-100 ">
       <h3 className="text-xl text-slate-950 mb-8 font-semibold border-b pb-4 border-slate-900 ">Upcoming Launches</h3>
-      {upcomingLaunches.slice().length > 0 && upcomingLaunches.slice(0).filter((launch, index) => index < 4).reverse().map((launch, index) => (
+      {upcomingLaunches.slice().length > 0 && upcomingLaunches.slice(0).reverse().filter((launch, index) => index < 4).reverse().map((launch, index) => (
         <div key={index} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
             <Image
