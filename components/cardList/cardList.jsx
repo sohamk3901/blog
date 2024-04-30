e9eb22ae7ce336029737cab58cd29b3744034ac3
Soham <63231620+sohamk3901@users.noreply.github.com>
2024-04-30 16:54:22 +0530

    Error correction 3

--- components/cardList/cardList.jsx
+++ components/cardList/cardList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./cardList.module.css";
-import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
 
